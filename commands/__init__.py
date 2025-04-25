c729c7bebbcaf951f13ee8886e4c6745b83957a1
Wout2126 <101658655+Wout2126@users.noreply.github.com>
2025-04-25 16:27:48 +0200

    Update __init__.py

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,1 +1,7 @@
-
+# Bind the commands to the tree explicitly
+self.bot.tree.add_command(self.help_command, guild=discord.Object(id=GUILD_ID))
+self.bot.tree.add_command(self.userinfo, guild=discord.Object(id=GUILD_ID))
+self.bot.tree.add_command(self.serverinfo, guild=discord.Object(id=GUILD_ID))
+self.bot.tree.add_command(self.ping, guild=discord.Object(id=GUILD_ID))
+self.bot.tree.add_command(self.vote, guild=discord.Object(id=GUILD_ID))
+self.bot.tree.add_command(self.status, guild=discord.Object(id=GUILD_ID))
