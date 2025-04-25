9c6e9c2e317c3bb2d147e6dbf325e11625bc81f2
Wout2126 <101658655+Wout2126@users.noreply.github.com>
2025-04-25 14:57:53 +0200

    Update user_commands.py

--- commands/user_commands.py
+++ commands/user_commands.py
@@ -16,12 +16,12 @@ class UserCommands(commands.Cog):
         self.bot = bot
         self.bot.start_time = datetime.datetime.utcnow()
 
-    @app_commands.command(name="help", description="Show help for all commands")
+@app_commands.command(name="help", description="Show help for all commands")
     async def help_command(self, interaction: discord.Interaction):
         help_text = load_help_text()
         await interaction.response.send_message(help_text)
 
-    @app_commands.command(name="userinfo", description="Get information about yourself")
+@app_commands.command(name="userinfo", description="Get information about yourself")
     async def userinfo(self, interaction: discord.Interaction):
         user = interaction.user
         embed = discord.Embed(title=f"{user.name}'s Info", color=discord.Color.blue())
@@ -34,7 +34,7 @@ class UserCommands(commands.Cog):
             embed.add_field(name="Roles", value=", ".join([role.name for role in member.roles[1:]]), inline=False)
         await interaction.response.send_message(embed=embed)
 
-    @app_commands.command(name="serverinfo", description="Get information about the server")
+@app_commands.command(name="serverinfo", description="Get information about the server")
     async def serverinfo(self, interaction: discord.Interaction):
         guild = interaction.guild
         embed = discord.Embed(title=f"{guild.name} Server Info", color=discord.Color.green())
@@ -45,12 +45,12 @@ class UserCommands(commands.Cog):
         embed.add_field(name="Roles", value=", ".join([role.name for role in guild.roles[1:]]), inline=False)
         await interaction.response.send_message(embed=embed)
 
-    @app_commands.command(name="ping", description="Check the bot's latency")
+@app_commands.command(name="ping", description="Check the bot's latency")
     async def ping(self, interaction: discord.Interaction):
         latency = round(self.bot.latency * 1000)
         await interaction.response.send_message(f"Pong! Latency is {latency}ms")
 
-    @app_commands.command(name="vote", description="Create a poll with up to 5 options")
+@app_commands.command(name="vote", description="Create a poll with up to 5 options")
     async def vote(self, interaction: discord.Interaction, question: str, option1: str, option2: str,
                    option3: str = None, option4: str = None, option5: str = None):
         options = [opt for opt in [option1, option2, option3, option4, option5] if opt]
@@ -69,7 +69,7 @@ class UserCommands(commands.Cog):
         for i in range(len(options)):
             await msg.add_reaction(emojis[i])
 
-    @app_commands.command(name="status", description="Show the bot's current status")
+@app_commands.command(name="status", description="Show the bot's current status")
     async def status(self, interaction: discord.Interaction):
         bot_uptime = datetime.datetime.utcnow() - self.bot.start_time
         embed = discord.Embed(title="Bot Status", color=discord.Color.purple())
